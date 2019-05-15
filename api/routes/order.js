48df3bb162112d69c351062a11288b40c6291754
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-05-15 14:18:53 +0500

    get order api

--- api/routes/order.js
+++ api/routes/order.js
@@ -5,8 +5,40 @@ const mongoose = require('mongoose');
 const Order = require('../models/order');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'get all order'
+    Order.find()
+    .select("quantity product _id")
+    .exec()
+    .then(docs => {
+        console.log("Get all Documents ", docs);
+
+        if (docs.length >= 0) {
+            const response = {
+                count: docs.length,
+                orders: docs.map(doc => {
+                    return {
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:5000/order/' + doc._id
+                        }
+                    }
+                })
+            }
+            res.status(200).json(response);
+        } else {
+            res.status(404).json({
+                message: 'no entries found'
+            });
+        }
+
+    })
+    .catch(err => {
+        console.log('error ', err);
+        res.status(500).json({
+            error: err
+        });
     })
 })
 

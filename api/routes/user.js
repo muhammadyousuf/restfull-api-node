63ce301c9d00ec35490020cbfb2f48bb0dc98067
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-20 16:07:42 +0500

    add validation user created

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,28 +5,38 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
 router.post('/signup', (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            res.status(500).json({ error: err })
+    User.find({ email: req.body.email }).exec().then(user => {
+        if (user.length >= 1) {
+            res.status(409).json({
+                message: 'email is already exsists'
+            })
         } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                    res.status(500).json({ error: err })
+                } else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash
+                    })
+                    user.save().then(result => {
+                        console.log(result);
+                        res.status(201).json({
+                            message: 'user created'
+                        })
+                    }).catch(err => {
+                        res.status(500).json({
+                            error: err
+                        })
+                    })
+                }
             })
-            user.save().then(result => {
-                console.log(result);
-                res.status(201).json({
-                    message: 'user created'
-                })
-            }).catch(err => {
-                res.status(500).json({
-                    error: err
-                })
-            })
+
         }
     })
 
 })
+
 
 module.exports = router;

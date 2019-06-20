3f0113086c06ea30735e31707eb1b57e6520542f
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-20 19:46:03 +0500

    get user listing

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 router.post('/signup', (req, res, next) => {
     User.find({ email: req.body.email }).exec().then(user => {
@@ -38,15 +38,27 @@ router.post('/signup', (req, res, next) => {
 
 })
 
+router.get('/list', (req, res, next) => {
+    User.find().exec().then(result => {
+        if (result.length >= 1) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ message: 'No User Account' })
+        }
+    }).catch(err => {
+        res.status(500).json({ error: err })
+    })
+})
+
 router.delete('/delete/:userId', (req, res, next) => {
     let uid = req.params.userId;
-    User.find({_id:uid}).exec().then(doc => {
+    User.find({ _id: uid }).exec().then(doc => {
         console.log(doc.length)
-        if(!doc.length){
-        res.status(404).json({
-            message:"No User Found"
-        })
-        }else{
+        if (!doc.length) {
+            res.status(404).json({
+                message: "No User Found"
+            })
+        } else {
             User.deleteOne({ _id: uid }).exec().then(() => {
                 res.status(200).json({
                     message: 'user deleted Successfully'
@@ -58,6 +70,6 @@ router.delete('/delete/:userId', (req, res, next) => {
             })
         }
     })
-   
+
 })
 module.exports = router;

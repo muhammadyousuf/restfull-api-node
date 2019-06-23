1a374c9e694d71decd089054af4fbfd692de4ca6
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-23 19:01:08 +0500

    product model controller completed

--- api/controller/product.js
+++ api/controller/product.js
@@ -1,6 +1,5 @@
 const Product = require('../models/product');
-
-
+const mongoose = require('mongoose');
 
 exports.get_all_product = (req, res, next) => {
     Product.find().sort({ name: 1 })
@@ -41,6 +40,37 @@ exports.get_all_product = (req, res, next) => {
         })
 }
 
+exports.create_product =  (req, res, next) => {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price,
+        productImage:req.file.path
+    })
+    product.save()
+        .then(result => {
+            console.log('result', result);
+            res.status(200).json({
+                message: 'Product Created SuccessFully',
+                product: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    productImage: result.productImage,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:5000/product/' + result._id
+                    }
+                }
+            })
+        }).catch(err => {
+            console.log("error", err);
+            res.status(500).json({ error: err })
+
+        })
+
+}
+
 exports.get_single_product =  (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id).select("name price _id productImage").exec().then(doc => {
@@ -57,6 +87,44 @@ exports.get_single_product =  (req, res, next) => {
             res.status(404).json({ message: 'No Valid Entry For Provided ID ' });
         }
 
+    }).catch(err => {
+        console.log('error', err);
+        res.status(500).json({ error: err })
+    })
+}
+
+exports.delet_single_product = (req, res, next) => {
+    const id = req.params.productId;
+    Product.remove({ _id: id }).exec().then(result => {
+        res.status(200).json({
+            message: 'Deleted Product SuccessFully',
+            request: {
+                type: 'POST',
+                url: 'http://localhost:5000/product',
+                body: { name: 'String', price: 'Number' }
+            }
+        })
+    }).catch(err => {
+        console.log('error', err);
+        res.status(500).json({ error: err })
+    })
+}
+
+exports.update_single_product =  (req, res, next) => {
+    const id = req.params.productId;
+    const updateOpt = {};
+    for (const opt of req.body) {
+        updateOpt[opt.Propname] = opt.value;
+    }
+    Product.update({ _id: id }, { $set: updateOpt }).exec().then(result => {
+        console.log('update', result);
+        res.status(200).json({
+            message: 'Update Record Successfully',
+            request: {
+                type: 'GET',
+                url: 'http://localhost:5000/product/' + id
+            }
+        });
     }).catch(err => {
         console.log('error', err);
         res.status(500).json({ error: err })

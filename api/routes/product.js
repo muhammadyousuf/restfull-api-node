91dc58f5258025dbd3a84ef6218eee827c6b2b6e
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-23 18:50:44 +0500

    get product controller

--- api/routes/product.js
+++ api/routes/product.js
@@ -35,45 +35,9 @@ const upload = multer({
 })
 
 const Product = require('../models/product');
+const Product_Controller = require('../controller/product');
 
-router.get('/', (req, res, next) => {
-    Product.find().sort({ name: 1 })
-        .select("name price _id productImage")
-        .exec()
-        .then(docs => {
-            console.log("Get all Documents ", docs);
-
-            if (docs.length >= 0) {
-                const response = {
-                    count: docs.length,
-                    product: docs.map(doc => {
-                        return {
-                            name: doc.name,
-                            price: doc.price,
-                            _id: doc._id,
-                            productImage: doc.productImage,
-                            request: {
-                                type: 'GET',
-                                url: 'http://localhost:5000/product/' + doc._id
-                            }
-                        }
-                    })
-                }
-                res.status(200).json(response);
-            } else {
-                res.status(404).json({
-                    message: 'no entries found'
-                });
-            }
-
-        })
-        .catch(err => {
-            console.log('error ', err);
-            res.status(500).json({
-                error: err
-            });
-        })
-})
+router.get('/', Product_Controller.get_all_product)
 router.post('/' , checkAuth, upload.single('productImage') ,  (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
@@ -104,27 +68,7 @@ router.post('/' , checkAuth, upload.single('productImage') ,  (req, res, next) =
         })
 
 })
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    Product.findById(id).select("name price _id productImage").exec().then(doc => {
-        console.log("Form Database", doc);
-        if (doc) {
-            res.status(200).json({
-                product: doc,
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:5000/product'
-                }
-            });
-        } else {
-            res.status(404).json({ message: 'No Valid Entry For Provided ID ' });
-        }
-
-    }).catch(err => {
-        console.log('error', err);
-        res.status(500).json({ error: err })
-    })
-})
+router.get('/:productId', Product_Controller.get_single_product)
 
 router.patch('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;

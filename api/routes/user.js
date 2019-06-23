5c3b30021e327804d2d70dc1a1d0ef75522adb0f
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-23 18:42:47 +0500

    remove extra imports

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-dotenv.config({ path: '../../nodemon.env' });
+
 const checkAuth = require('../middleware/check-auth');
 const User_Controller = require('../controller/user')
 
@@ -13,7 +8,9 @@ router.post('/signup', User_Controller.create_user)
 
 router.post('/Login', User_Controller.User_Login)
 
-router.delete('/delete/:userId', checkAuth , User_Controller.User_Delete)
+router.delete('/delete/:userId', checkAuth, User_Controller.User_Delete)
 
 router.get('/allUser', checkAuth, User_Controller.list_all_users)
+
+
 module.exports = router;

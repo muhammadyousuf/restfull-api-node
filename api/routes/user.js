8cf8388e75e24b6663560a523363b5fd8bd05aec
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-23 18:41:30 +0500

    user model controller completed

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,79 +6,14 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config({ path: '../../nodemon.env' });
-
+const checkAuth = require('../middleware/check-auth');
 const User_Controller = require('../controller/user')
 
 router.post('/signup', User_Controller.create_user)
 
-router.post('/Login', (req, res, next) => {
-    User.find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if (user.length < 1) {
-                res.status(401).json({ message: "Auth Failed" })
-            } else {
-                bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-                    if (err) {
-                        res.status(401).json({ message: "Auth Failed" })
-                    }
-                    if (result) {
-                        const token = jwt.sign(
-                            {
-                                email: user[0].email,
-                                userId: user[0]._id
-                            },
-                            process.env.JWT_KEY,
-                            {
-                                expiresIn: '1d'
-                            }
+router.post('/Login', User_Controller.User_Login)
 
-                        )
-                        res.status(200).json({ message: "Auth Successful", token })
-                    } else {
-                        res.status(401).json({ message: "Auth Failed" })
-                    }
-                })
-            }
-        })
-        .catch(err =>
-            res.status(500).json({
-                error: err
-            }))
-})
+router.delete('/delete/:userId', checkAuth , User_Controller.User_Delete)
 
-router.delete('/delete/:userId', (req, res, next) => {
-    let uid = req.params.userId;
-    User.find({ _id: uid }).exec().then(doc => {
-        console.log(doc.length)
-        if (!doc.length) {
-            res.status(404).json({
-                message: "No User Found"
-            })
-        } else {
-            User.deleteOne({ _id: uid }).exec().then(() => {
-                res.status(200).json({
-                    message: 'user deleted Successfully'
-                })
-            }).catch(err => {
-                res.status(500).json({
-                    error: err
-                })
-            })
-        }
-    })
-})
-
-router.get('/allUser', (req, res, next) => {
-    User.find().exec().then(doc => {
-        if (doc.length >= 1) {
-            res.status(200).json(doc)
-        } else {
-            res.status(404).json({ message: 'No User Account' })
-        }
-    })
-        .catch(err => {
-            res.status(500).json({ error: err })
-        })
-})
+router.get('/allUser', checkAuth, User_Controller.list_all_users)
 module.exports = router;
